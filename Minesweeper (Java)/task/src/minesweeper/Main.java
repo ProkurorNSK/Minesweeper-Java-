f7817af2ff036473532ec7e0ad7a0be5788e3d35
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-03-01 02:59:58 +0700

    2

--- Minesweeper (Java)/task/src/minesweeper/Main.java
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -1,16 +1,44 @@
 package minesweeper;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    final static int height = 10;
+    final static int width = 10;
+    final static char[][] field = new char[height][width];
     public static void main(String[] args) {
-        System.out.println("""
-                .X.......
-                .....X..X
-                ....X....
-                ......X..
-                ..X......
-                ....X....
-                ..X......
-                ..X......
-                ......X..""");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many mines do you want on the field?");
+        init(sc.nextInt());
+        print();
+    }
+
+    private static void print() {
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                System.out.print(field[j][i]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void init(int number) {
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                field[j][i] = '.';
+            }
+        }
+
+        Random random = new Random();
+        for (int i = 0; i < number; i++) {
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            if (field[y][x] == 'X') {
+                i--;
+            } else {
+                field[y][x] = 'X';
+            }
+        }
     }
 }
